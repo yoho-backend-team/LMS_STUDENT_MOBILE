b415eb905e2f03e1d1056897e6372964881efe2d
suryas-1805 <suryas18051998@gmail.com>
2025-08-25 16:44:10 +0530

    changes updated

--- App.tsx
+++ App.tsx
@@ -11,7 +11,6 @@ export default function App() {
       <Provider store={store}>
         <SafeAreaProvider>
           <NavigationContainer>
-            
             <Routes />
             <Toast />
           </NavigationContainer>
