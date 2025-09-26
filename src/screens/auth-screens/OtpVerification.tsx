7ce120c1456ae222658ab4f255369a487a5b4106
suryas-1805 <suryas18051998@gmail.com>
2025-09-26 11:10:20 +0530

    updated code

--- src/screens/auth-screens/OtpVerification.tsx
+++ src/screens/auth-screens/OtpVerification.tsx
@@ -115,7 +115,10 @@ const OTPVerification: React.FC<OTPVerificationProps> = ({ route }) => {
     const otpString = otp.join('');
     if (otpString.length === 6) {
       try {
-        const response = await updateVerifyOtpClient({ email, token: data?.token, otp: otpString });
+        const params_data = { email, token: data?.token, otp: otpString };
+        console.log(params_data, 'params');
+        const response = await updateVerifyOtpClient(params_data);
+        console.log(response, 'otp verify res');
         if (response) {
           toast.success('Success', `OTP Verified successfully!`);
           if (data?.step === 'otp') {
